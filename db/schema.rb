1b86d4de9db0be134e16d74ae7e7ff7e36c9a44d
K Faiqoh <kfaiqoh@outlook.com>
2019-10-11 13:00:39 +0700

    database migration

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_10_08_142501) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
